81296d4daaa556748c83db49c6d8876becd81c1c
Swe Mon Aung <>
2016-10-22 15:56:23 +0800

    Added blank resource

---
+++ src/java/org/nus/web/rest/resource/AppointmentResource.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nus.web.rest.resource;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author swemon
+ */
+@RequestScoped
+@Path("/appointment")
+public class AppointmentResource {
+    
+    @Path("{aid}")
+    public Response getAppointment(@PathParam("aid") Integer id) {
+        // TODO implementation
+        return Response.ok("Inside AppointmentResource").build();
+    }
+    
+}
---
+++ src/java/org/nus/web/rest/resource/PeopleResource.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nus.web.rest.resource;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author swemon
+ */
+@RequestScoped
+@Path("/people")
+public class PeopleResource {
+
+    @POST
+    public Response post() {
+        
+        // TODO Implementation here
+        return Response.ok("Inside people resource POST").build();
+    }
+     
+    @GET
+    public Response findPeople() {
+        // TODO Implementation here
+        return Response.ok("Inside people resource GET").build();
+    }
+}
