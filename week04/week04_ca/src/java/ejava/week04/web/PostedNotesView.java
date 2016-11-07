70b6ddf3a9c46556344cf79d9a33add1fae3503d
Swe Mon Aung <>
2016-11-07 23:38:34 +0800

    Minor change

--- week04/week04_ca/src/java/ejava/week04/web/PostedNotesView.java
+++ week04/week04_ca/src/java/ejava/week04/web/PostedNotesView.java
@@ -7,18 +7,11 @@ package ejava.week04.web;
 
 import ejava.week04.bean.NoteBean;
 import ejava.week04.entity.Notes;
-import ejava.week04.entity.Users;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collection;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -29,83 +22,15 @@ import javax.servlet.http.HttpServletRequest;
 @Named
 public class PostedNotesView implements Serializable {
   
-    @Inject
-            
+    @Inject 
     NoteBean noteBean;
+    
     private static final long serialVersionUID = 1L;
-    
-    private int noteid;
-    private String title;
-    private String category;
-    private String content;
-    private String userid;
-
-    private Collection<Notes> noteList;
-    
-
-    public NoteBean getNoteBean() {
-        return noteBean;
-    }
-
-    public void setNoteBean(NoteBean noteBean) {
-        this.noteBean = noteBean;
-    }
-
-    public int getNoteid() {
-        return noteid;
-    }
-
-    public void setNoteid(int noteid) {
-        this.noteid = noteid;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public Collection<Notes> getNoteList() {
-        return noteList;
-    }
-
-    public void setNoteList(Collection<Notes> noteList) {
-        this.noteList = noteList;
-    }
-    
       
-    public String getPostedNotes() {
-
-           
-        Collection<Notes> noteList = noteBean.findAllNotes();
+    public Collection<Notes> getPostedNotes() {
+         Collection<Notes> noteList = noteBean.findAllNotes();
              
-        setNoteList(noteList);/* Important to set data after retrieving from DB. */
+       //setNoteList(noteList);/* Important to set data after retrieving from DB. */
         
         if (noteList.size() > 0) {
             System.out.println("Database connection successful ");
@@ -121,7 +46,7 @@ public class PostedNotesView implements Serializable {
             System.out.println(note.getContent()+"\r\n");
         }
 
-        return ("/secure/postedNotes.xhtml");
+        return noteList;
                      
     }
     
