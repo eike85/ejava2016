153aa9a020d27804e3d37808a7eb02eada0a2003
Swe Mon Aung <>
2016-11-09 23:32:58 +0800

    Add more events

--- week04/week04_ca/web/js/noticeboard.js
+++ week04/week04_ca/web/js/noticeboard.js
@@ -2,9 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var notes = [];
 $(function() {
     var socket;
+    
+    var filerByCategory = function() {
+        $("#location tr").remove();
+        var trHTML = '';
+        $.each(notes, function (i, item) {
+            var filterCatgory = $('#selectId').val();
+            if ( filterCatgory === 'All' || item.category === filterCatgory ) {
+              trHTML += '<tr><td>' + item.title + '</td><td>' + item.category + '</td><td>' + item.content + '<td>' + item.userid + '</td><td>' + item.postedDate + '</td>/td></tr>';
+            }
+        });
+
+        $('#location').append(trHTML);
+    };
+    
+    //var notes = [];
     //$( "#connect" ).on("click", function(){
        if ( !socket ) {
            socket = new WebSocket("ws://localhost:8080/week04_ca/board");
@@ -19,31 +34,33 @@ $(function() {
            socket.onmessage = function(evt) {
                 var json = JSON.parse(evt.data);
                 console.log(json);
-
-                var tr;
+                
                 for (var i = 0; i < json.length; i++) {
-                    tr = $('<tr/>');
-                    tr.append("<td>" + json[i].userid + "</td>");
-                    tr.append("<td>" + json[i].title + "</td>");
-                    tr.append("<td>" + json[i].content + "</td>");
-                    tr.append("<td>" + json[i].posteddate + "</td>");
-                    tr.append("<td>" + json[i].noteid + "</td>");
-                    $('table').append(tr);
+                    var note = {
+                        "title": "",
+                        "userid": "",
+                        "category": "",
+                        "content": "",
+                        "postedDate": ""
+                    };
+                    note.title = json[i].title;
+                    note.category = json[i].category; 
+                    note.content = json[i].content; 
+                    note.userid = json[i].userid; 
+                    note.postedDate = json[i].postedDate; 
+                    notes.push(note);
                 }
+                filerByCategory();
            };
        }
-//       else {
-//           alert ("Socket is null! Closing...");
-//           socket.close();
-//           socket = null;
-//       }
-    //});
     
     $( "#all" ).on("click", function(){
         socket.send("All");
         console.log("Sending all");
     });
-   
+    
+    $('#selectId').on('change', function() {
+        console.log(this.value);
+        filerByCategory();
+    });
 });
-
-
